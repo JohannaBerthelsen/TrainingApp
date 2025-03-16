035efae9da778466d4515004a3bc9ec94d780e5f
jolobert <jolbitu.dk>
2025-03-16 18:50:09 +0100

    added navbar component to app.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
+import Navbar from "./components/Navbar";
 import StartPage from "./pages/StartPage";
 
 function App() {
   return (
     <Router>
     <GlobalStyle/>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<StartPage/>} />
       </Routes>
