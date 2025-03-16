0b92aeb01944da708d2bc5279d93202afd0f8d96
jolobert <jolbitu.dk>
2025-03-16 18:05:39 +0100

    added router to app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./globalStyles";
 import StartPage from "./pages/StartPage";
 
 function App() {
   return (
-      <StartPage/>
+    <Router>
+    <GlobalStyle/>
+      <Routes>
+        <Route path="/" element={<StartPage/>} />
+      </Routes>
+    </Router>
+  
   );
 }
 
