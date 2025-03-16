b06d83ee1866a41525e1e0d031f710a45b06a8ce
jolobert <jolbitu.dk>
2025-03-16 18:50:26 +0100

    started working on navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledNavbar = styled.nav`    
+`;
+
+function Navbar(){
+    return(
+        <StyledNavbar>
+
+        </StyledNavbar>
+    );
+}
+export default Navbar;
