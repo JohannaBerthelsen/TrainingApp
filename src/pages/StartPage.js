1888ab054ce574b4633154752e96221a1e2a59f2
jolobert <jolbitu.dk>
2025-03-16 18:11:46 +0100

    added styling to start page

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,7 +1,16 @@
+import styled from "styled-components";
+
 function StartPage(){
     return(
-        <h1>Welcome to Johanna's Training App</h1>
+        <StyledText>
+            <h1>Welcome to Johanna's Training App</h1>
+        </StyledText>
     );
 }
 
 export default StartPage;
+
+
+const StyledText = styled.div`
+    text-align: center;
+`;
