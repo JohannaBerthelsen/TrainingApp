21275ea31cd1de9e9c603dbcd5d68bed3230eebe
jolobert <jolbitu.dk>
2025-04-04 22:59:04 +0200

    added test request

--- frontend/src/pages/StartPage.js
+++ frontend/src/pages/StartPage.js
@@ -1,16 +1,35 @@
 import styled from "styled-components";
 import logo from "../assets/logo.png";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function StartPage(){
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:5001/users")
+            .then(response => {
+                setUsers(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching users:", error);
+            });
+    }, []);
+
     return(
         <Container>
             <Text>
                 <h1>Welcome to Johanna's Training App</h1>
+                <UserList>
+                    {users.map((user, index) => (
+                        <UserItem key={index}>
+                            👤 {user.name} - {user.email}
+                        </UserItem>
+                    ))}
+                </UserList>
             </Text>
             <Image src={logo} />
         </Container>
-
-
     );
 }
 
@@ -35,4 +54,15 @@ const Container = styled.div`
     align-items: center;
 
 `
+
+const UserList = styled.ul`
+    margin-top: 20px;
+    list-style: none;
+    padding: 0;
+`;
+
+const UserItem = styled.li`
+    font-size: 16px;
+    margin-bottom: 10px;
+`;
 
