a740c0331b445d289d579d42e3b0a1f6d3b32949
jolobert <jolbitu.dk>
2025-04-04 22:59:31 +0200

    adding information to backend to connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const mysql = require("mysql2");
+const cors =require("cors");
+
 const PORT = 5001;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "klarinett",
+    database: "training_db"
+});
+
+app.get("/users", (req, res) => {
+    const sql = "SELECT * FROM users";
+    db.query(sql, (err, results) => {
+      if (err) return res.status(500).json({ error: err });
+      res.json(results);
+    });
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
