6d1939c411b4f6d8163e869be5b679aa05bf2877
jolobert <jolbitu.dk>
2025-04-05 14:30:43 +0200

    Added routes to new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
 import Navbar from "./components/Navbar";
 import StartPage from "./pages/StartPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
     <Navbar/>
       <Routes>
         <Route path="/" element={<StartPage/>} />
+        <Route path="/register" element={<RegistrationPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
       </Routes>
     </Router>
   
